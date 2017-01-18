89353edd19e9758701293b38ffce19dc16f46526
Tihonya <mrTihonya@gmail.com>
2017-01-18 06:09:09 +0200

    Implement UnGoing gig API [HttpDelete] & JS.Ajax
    1)Written JQuery Ajax method(:DELETE) . through a condition:
    "if (el.hasClass("btn")){}" (new for me :/)
    2)Written Attendences Delete API method and check routing.

--- GigHub.Web/Controllers/Api/AttendancesController.cs
+++ GigHub.Web/Controllers/Api/AttendancesController.cs
@@ -1,9 +1,9 @@
-using System.Linq;
-using System.Web.Http;
 using GigHub.Core;
 using GigHub.DataLayer;
 using GigHub.Web.Dtos;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
 
 namespace GigHub.Web.Controllers.Api
 {
@@ -40,5 +40,27 @@ namespace GigHub.Web.Controllers.Api
 
             return Ok();
         }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+            var attendances = _context.Attendances.ToList();
+
+            var attendance = attendances
+                .SingleOrDefault(a => a.AttendeeId == userId
+                                      && a.GigId == dto.GigId);
+
+            if (attendance == null) { 
+                return NotFound();
+            }
+
+            _context.Attendances.Remove(attendance);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
     }
 }
